b0023b24b714bbf2d20d0eaf2196b1009e4f3c31
Razvan <tangent.jotey@gmail.com>
2021-10-24 21:15:54 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import MyComponent from './lib/components/MyComponent.svelte'
+const MyOtherComponent = import('./lib/components/MyOtherComponent.svelte')
+</script>
+
+<MyComponent/>
+{#await MyOtherComponent} <!-- wait for it to load --->
+  Other component is loading...
+{:then MyOtherComponent}
+  <MyOtherComponent.default/>
+{/await}
